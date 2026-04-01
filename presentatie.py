bcf28e72e41768779c9e761792791bee72679a02
Gerrit-kjeld Dusseljee <PunkPony@proton.me>
2026-04-01 13:52:15 +0200

    Les 10 huiswerk

---
+++ presentatie.py
@@ -0,0 +1,7 @@
+def presenteer(dictionary,totaal)
+    for key, value in dictionary.items():
+        print(f"{key} : {value} euro")
+
+    print(26 * "=")
+    print(f"Totaal : {totaal}")
+    return()
